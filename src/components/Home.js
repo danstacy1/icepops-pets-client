0a48cf5dfaaaf52c5be453123ab1e48251b41817
danstacy1 <danstacy1@gmail.com>
2022-07-29 13:25:58 -0400

    added spinning circle

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
+import PetsIndex from './pets/PetsIndex'
+
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
 
 	return (
 		<>
-			<h2>Home Page</h2>
+			<h2>See the Pets</h2>
+			<PetsIndex />
 		</>
 	)
 }
