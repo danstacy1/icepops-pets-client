53f13f91bd2d4d4b893981b820ca654e9a584029
danstacy1 <danstacy1@gmail.com>
2022-11-14 21:35:00 -0500

    lined up App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import SignUp from './components/auth/SignUp'
 import SignIn from './components/auth/SignIn'
 import SignOut from './components/auth/SignOut'
 import ChangePassword from './components/auth/ChangePassword'
+import ShowPet from './components/pets/ShowPet'
+// import PetForm from './components/shared/PetForm'
+import CreatePet from './components/pets/CreatePet'
 
 const App = () => {
 
@@ -53,21 +56,33 @@ const msgAlert = ({ heading, message, variant }) => {
 					path='/sign-in'
 					element={<SignIn msgAlert={msgAlert} setUser={setUser} />}
 				/>
-		<Route
-		path='/sign-out'
-		element={
-			<RequireAuth user={user}>
-			<SignOut msgAlert={msgAlert} clearUser={clearUser} user={user} />
-			</RequireAuth>
-		}
-		/>
-		<Route
-		path='/change-password'
-		element={
-			<RequireAuth user={user}>
-			<ChangePassword msgAlert={msgAlert} user={user} />
-			</RequireAuth>}
-		/>
+				<Route
+				path='/sign-out'
+				element={
+					<RequireAuth user={user}>
+					<SignOut msgAlert={msgAlert} clearUser={clearUser} user={user} />
+					</RequireAuth>
+				}
+				/>
+				<Route
+				path='/change-password'
+				element={
+					<RequireAuth user={user}>
+					<ChangePassword msgAlert={msgAlert} user={user} />
+					</RequireAuth>}
+				/>
+				<Route
+							path="/pets/:id"
+							element={ <ShowPet user={ user } msgAlert={ msgAlert } />}				
+				/>
+				<Route
+					path="/addPet"
+					element={
+						<RequireAuth user={ user }>
+							<CreatePet msgAlert={msgAlert} user={user} />
+						</RequireAuth>  
+					}
+				/>
 			</Routes>
 			{msgAlerts.map((msgAlert) => (
 				<AutoDismissAlert
